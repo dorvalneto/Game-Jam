6bf69f472e5cc605a8e1399de6b8fb68e29894e3
Nareilson <narotkd@gmail.com>
2020-04-03 23:43:25 -0300

    Iniciando o projeto com estrutura basica incompleta

---
+++ Assets/Scripts/CobrinhaScripts/Player/CobrinhaController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CobrinhaController : MonoBehaviour
+{
+    public float speed = 5f;
+    private MovimentacaoUtils _movimentacaoUtils;
+    private KeyBoardUtils _boardUtils;
+    public Rigidbody2D corpoRigido;
+    private List<Transform> _listCorpo;
+    void Start()
+    {
+        corpoRigido = GetComponent<Rigidbody2D>();
+        _movimentacaoUtils = new MovimentacaoUtils();
+        _boardUtils = new KeyBoardUtils();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movimentacaoUtils.movieUtils(_boardUtils.actionKeyBoard(),corpoRigido,speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("comida"))
+        {
+            
+        }
+        
+    }
+
+    private void movieLogic()
+    {
+        if (_listCorpo != null && _listCorpo.Count > 0)
+        {
+            _listCorpo.Last().transform.position = transform.position;
+            _listCorpo.Insert(0,_listCorpo.Last());
+            _listCorpo.RemoveAt(_listCorpo.Count - 1);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/CobrinhaScripts/Player/Utils/KeyBoardUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBoardUtils : MonoBehaviour
+{
+   public MOVIE actionKeyBoard()
+   {
+      if (Input.GetKey(KeyCode.UpArrow))
+      {
+         return MOVIE.Cima;
+      }
+
+      if (Input.GetKey(KeyCode.DownArrow))
+      {
+         return MOVIE.Baixo;
+      }
+
+      if (Input.GetKey(KeyCode.LeftArrow))
+      {
+         return MOVIE.Esquerda;
+      }
+
+      if (Input.GetKey(KeyCode.RightArrow))
+      {
+         return MOVIE.Direita;
+      }
+      return MOVIE.Parado;
+   }
+}
