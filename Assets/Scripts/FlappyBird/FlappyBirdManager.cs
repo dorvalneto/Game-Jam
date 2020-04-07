649dd6217a1fb80872f74feba6dc001fb7db0e23
Dorval <dorvaldfneto@gmail.com>
2020-04-06 23:07:29 -0300

    Flappy bird

---
+++ Assets/Scripts/FlappyBird/FlappyBirdManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlappyBirdManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+
+    private void Start()    {
+        Time.timeScale = 1;
+    }
+
+    public void GameOver(){
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Replay(){
+        SceneManager.LoadScene("FlappyBirdScene");
+    }
+}
--- Assets/Scripts/Tetris/TetrisManager.cs
+++ Assets/Scripts/Tetris/TetrisManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TetrisManager : MonoBehaviour
 {
@@ -10,17 +11,31 @@ public class TetrisManager : MonoBehaviour
     public static int lines = 0;
     public Text textUI;
     public Image[] images;
+    public GameObject gameOverCanvas;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         textUI.text = string.Concat("Lines: " , lines.ToString() , " / " , totalLines.ToString());
+
+        if (lines == totalLines) {
+            GameOver();
+        }
+
+    }
+
+    private void GameOver() {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void LoadScene(string scene){
+        SceneManager.LoadScene(scene);
     }
 
     public void AddLine(int linecount){
